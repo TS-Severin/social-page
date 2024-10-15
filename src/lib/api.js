2d4bba288a3d52d32c4a5de051c095ab1915de6a
Tillmann Severin <tillmann.severin@posteo.de>
2024-10-15 22:34:07 +0200

    connection contentful page

---
+++ src/lib/api.js
@@ -0,0 +1,53 @@
+// contentmodel for posts
+
+const POST_GRAPHQL_FIELDS = `
+sys {
+  id
+}
+  profileUsername
+`;
+
+async function fetchGraphQL(query, preview = false) {
+  return fetch(
+    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // Switch the Bearer token depending on whether the fetch is supposed to retrieve live
+        // Contentful content or draft content
+        Authorization: `Bearer ${preview
+          ? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
+          : process.env.CONTENTFUL_ACCESS_TOKEN
+          }`,
+      },
+      body: JSON.stringify({ query }),
+      // Associate all fetches for articles with an "articles" cache tag so content can
+      // be revalidated or updated from Contentful on publish
+      next: { tags: ["post"] },
+
+    }
+  ).then((response) => response.json());
+}
+
+function extractPosts(fetchResponse) {
+  return fetchResponse?.data?.postCollection?.items;
+}
+
+
+export async function getPosts(
+  isDraftMode = false) {
+  const allPosts = await fetchGraphQL(
+    `query {
+        postCollection(preview: ${isDraftMode ? "true" : "false"
+    }) { 
+          items {
+            ${POST_GRAPHQL_FIELDS}
+          }
+        }
+      }`, isDraftMode
+  );
+  return extractPosts(allPosts);
+}
+
+
