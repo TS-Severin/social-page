5ec6c8a7cbd06fa6ceef66c8f374d2ac36544ea7
Tillmann Severin <tillmann.severin@posteo.de>
2024-10-27 19:02:10 +0100

    add posts on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
 import { getPosts } from "@/lib/api";
 
+// Define the type for each post object
+interface Post {
+  sys: {
+    id: string;
+  };
+  profileusername: string;
+}
+
 export default async function Home() {
-  const posts = await getPosts();
+  const posts: Post[] = await getPosts();
   console.log("all the posts ", posts);
 
   return (
-    <main>
-      <h1>hello world</h1>
-      {/* { {posts.profileusername.map((profileusername: string) => (
-   <h1 key={posts.sys.id}>{profileusername}</h1>
-      ))} } */}
-    </main>
+    <>
+      <main>
+        <h1>hello world</h1>
+        {/* Iterate over each post and render profileusername */}
+        {posts.map((post) => (
+          <h1 key={post.sys.id}>{post.profileusername}</h1>
+        ))}
+      </main>
+    </>
   );
 }
