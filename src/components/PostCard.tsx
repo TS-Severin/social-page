4a66f8f2bb602af30273461d86baee7ca6798280
Tillmann Severin <tillmann.severin@posteo.de>
2024-10-31 10:15:40 +0100

    render PostCard

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Post } from "../lib/types";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
@@ -9,10 +10,27 @@ export const PostCard: React.FC<PostCardProps> = ({ postsData }) => {
   return (
     <>
       {postsData.map((post) => (
-        <div key={post.sys.id}>
-          <h1>{post.profileusername}</h1>
-          <p>{post.hashtags}</p>
-          <p>{documentToReactComponents(post.posttext.json)}</p>
+        <div className="max-w-[600px] my-0 mx-auto">
+          <div className="relative top-20 mb-6 bg-white border border-gray-300 rounded shadow-sm">
+            <div className="flex items-center p-4 border-b border-gray-300">
+              <Image
+                src={post.profilepicture.url}
+                width={500}
+                height={500}
+                alt="Picture of the author"
+              ></Image>
+              <div className="text-base font-bold">{post.profileusername}</div>
+            </div>
+
+            <section className="relative overflow-hidden"></section>
+
+            <div className="p-[16px]">
+              <div>
+                <strong>spiced academy </strong>
+                {documentToReactComponents(post.posttext.json)}
+              </div>
+            </div>
+          </div>
         </div>
       ))}
     </>
@@ -31,7 +49,7 @@ export default PostCard;
               height={500}
               alt="Picture of the author"
             ></Image>
-            <div className="text-base font-bold">spiced academy</div>
+            <div className="text-base font-bold">{post.profileusername}</div>
           </div>
 
           <section className="relative overflow-hidden">
@@ -40,11 +58,7 @@ export default PostCard;
 
           <div className="p-[16px]">
             <p>
-              <strong>spiced academy </strong>Am 1. März 2024 habe ich an der
-              Spiced Academy mein Capstone Project "Erinnerungsfutur" für das
-              Web Development Bootcamp präsentiert - und wir haben mit unserer
-              Coriander Cohort die Graduation gefeiert. Danke an alle
-              großartigen teachers und die ganze cohort!
+              <strong>spiced academy </strong>{documentToReactComponents(post.posttext.json)}
             </p>
           </div>
         </div>
