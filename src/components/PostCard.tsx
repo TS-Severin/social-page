bd9b32f04766202adfefbcd2e6df8ffd59c8e9df
Tillmann Severin <tillmann.severin@posteo.de>
2024-11-01 12:19:42 +0100

    format rtf formatter rendering

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,37 +1,59 @@
 import Image from "next/image";
+import { Fragment } from "react";
 import { Post } from "../lib/types";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { INLINES } from "@contentful/rich-text-types";
 
 interface PostCardProps {
   postsData: Post[];
 }
 
+// makes the p take being showed inline to avoid a break between profilename and the posttext
+// makes the links in the posttext open links in a new tab
+const options = {
+  renderNode: {
+    paragraph: (node: any, children: any) => (
+      <p className="inline">{children}</p>
+    ),
+    [INLINES.HYPERLINK]: (node: any, children: any) => (
+      <a href={node.data.uri} target="_blank" rel="noopener noreferrer">
+        {children}
+      </a>
+    ),
+  },
+};
+
 export const PostCard: React.FC<PostCardProps> = ({ postsData }) => {
   return (
     <>
       {postsData.map((post) => (
-        <div className="max-w-[600px] my-0 mx-auto">
-          <div className="relative top-20 mb-6 bg-white border border-gray-300 rounded shadow-sm">
-            <div className="flex items-center p-4 border-b border-gray-300">
-              <Image
-                src={post.profilepicture.url}
-                width={500}
-                height={500}
-                alt="Picture of the author"
-              ></Image>
-              <div className="text-base font-bold">{post.profileusername}</div>
-            </div>
+        <Fragment key={post.sys.id}>
+          <div className="max-w-[600px] my-0 mx-auto">
+            <div className="relative top-20 mb-6 bg-white border border-gray-300 rounded shadow-sm">
+              <div className="flex items-center p-4 border-b border-gray-300">
+                <Image
+                  src={post.profilepicture.url}
+                  width={40}
+                  height={40}
+                  alt="Picture of the author"
+                  className="rounded-full mr-3"
+                ></Image>
+                <div className="text-base font-bold">
+                  {post.profileusername}
+                </div>
+              </div>
 
-            <section className="relative overflow-hidden"></section>
-
-            <div className="p-[16px]">
-              <div>
-                <strong>spiced academy </strong>
-                {documentToReactComponents(post.posttext.json)}
+              <section className="relative overflow-hidden"></section>
+              <h1>here comes the slider component</h1>
+              <div className="p-[16px]">
+                <div>
+                  <strong>spiced academy </strong>
+                  {documentToReactComponents(post.posttext.json, options)}
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </Fragment>
       ))}
     </>
   );
@@ -45,8 +67,8 @@ export default PostCard;
           <div className="flex items-center p-4 border-b border-gray-300">
             <Image
               src={post.profilepicture.url}
-              width={500}
-              height={500}
+              width={40}
+              height={40}
               alt="Picture of the author"
             ></Image>
             <div className="text-base font-bold">{post.profileusername}</div>
